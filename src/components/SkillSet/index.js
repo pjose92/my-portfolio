804adf5547883df88ce95c46fce3ecef1aebdd65
Jose <pjose14.jp38@yahoo.com>
2020-11-15 20:21:48 -0600

    added more skills icons

--- src/components/SkillSet/index.js
+++ src/components/SkillSet/index.js
@@ -5,8 +5,12 @@ import Wrapper from '../SectionWrapper';
 
 import "./App.css";
 import { IconContext } from "react-icons";
-import { FaHtml5, FaCss3Alt, FaReact, FaDatabase, FaPython, FaLess, FaBootstrap } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaReact, FaDatabase, FaPython, FaLess, FaBootstrap, FaNode, FaGithub } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
+import { SiPostman, SiPostgresql } from "react-icons/si";
+import { DiGit } from "react-icons/di";
+
+
 
 class SkillSet extends Component {
   constructor() {
@@ -36,8 +40,23 @@ class SkillSet extends Component {
               <FaBootstrap />
               <IoLogoJavascript />
               <FaReact />
+            </IconContext.Provider>
+          </div>
+
+          <div className="icons-about">
+            <IconContext.Provider value={{ size: "2.5em", color: "#474242" }}>
               <FaDatabase />
               <FaPython />
+              <FaNode />
+              <SiPostman />
+              <SiPostgresql />
+            </IconContext.Provider>
+          </div>
+
+          <div className="icons-about">
+            <IconContext.Provider value={{ size: "2.5em", color: "#474242" }}>
+              <FaGithub />
+              <DiGit />
             </IconContext.Provider>
           </div>
       </Wrapper>
