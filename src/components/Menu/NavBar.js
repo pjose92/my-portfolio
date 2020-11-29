5406349a5ea3aafec011ad80e970f69fa28dd117
Jose <pjose14.jp38@yahoo.com>
2020-11-28 18:03:05 -0600

    updated nav bar. still needs more work

--- src/components/Menu/NavBar.js
+++ src/components/Menu/NavBar.js
@@ -1,64 +1,93 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import styled from "styled-components";
+// import '../Menu/style.css'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//     </div>
+//   );
+// }
+
+// class Navbar extends Component {
+//   state = {
+//     opened: false
+//   };
+
+//   toggle() {
+//     this.setState({
+//       opened: !this.state.opened
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <Container className="Navbar">
+//         <div className="navbar-home">
+//           <a href="#home">
+//             DevJP
+//           </a>
+//           <button className="toggle" onClick={this.toggle.bind(this)}>
+//             <i
+//               className={
+//                 (this.state.opened ? "fa fa-caret-up" : "fa fa-caret-down")
+//               }
+//             />
+//           </button>
+//         </div>
+//         <ul
+//           className={
+//             "navbar-links " + (this.state.opened ? "opened" : "closed")
+//           }
+//         >
+//           <li className="navbar-link">
+//             <a href="#home">Home</a>
+//           </li>
+//           <li className="navbar-link">
+//             <a href="#about">About</a>
+//           </li>
+//           <li className="navbar-link">
+//             <a href="#skillset">Skills</a>
+//           </li>
+//           <li className="navbar-link">
+//             <a href="#projects">Work</a>
+//           </li>
+//         </ul>
+//       </Container>
+//     );
+//   }
+// }
+
+// const Container = styled.nav``;
+
+// export default App;
+
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from "react";
 import styled from "styled-components";
-import '../Menu/style.css'
+import Burger from "./Burger";
 
-function App() {
+const Nav = styled.nav`
+  width: 100%;
+  height: 55px;
+  border-bottom: 2px solid #f1f1f1;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  .logo {
+    padding: 15px 0;
+  }
+`;
+
+const Navbar = () => {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Nav>
+      <div className="logo">DevJP</div>
+      <Burger />
+    </Nav>
   );
-}
+};
 
-class Navbar extends Component {
-  state = {
-    opened: false
-  };
+export default Navbar;
 
-  toggle() {
-    this.setState({
-      opened: !this.state.opened
-    });
-  }
-
-  render() {
-    return (
-      <Container className="Navbar">
-        <div className="navbar-home">
-          <a href="#home">
-            DevJP
-          </a>
-          <button className="toggle" onClick={this.toggle.bind(this)}>
-            <i
-              className={
-                (this.state.opened ? "fa fa-caret-up" : "fa fa-caret-down")
-              }
-            />
-          </button>
-        </div>
-        <ul
-          className={
-            "navbar-links " + (this.state.opened ? "opened" : "closed")
-          }
-        >
-          <li className="navbar-link">
-            <a href="#home">Home</a>
-          </li>
-          <li className="navbar-link">
-            <a href="#about">About</a>
-          </li>
-          <li className="navbar-link">
-            <a href="#skillset">Skills</a>
-          </li>
-          <li className="navbar-link">
-            <a href="#projects">Work</a>
-          </li>
-        </ul>
-      </Container>
-    );
-  }
-}
-
-const Container = styled.nav``;
-
-export default App;
